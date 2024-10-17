67d3cb83e036c9f0a75dc01962d98e3f9d56058e
Kamuela Dietiker <kamuela.dietiker@paidy.com>
2024-10-17 11:14:47 +0900

    add library fix

--- home.nix
+++ home.nix
@@ -32,6 +32,9 @@
 
     bash = {
       enable = true;
+      initExtra = ''
+      export NEXUS_HOST_RELEASES=https://nexus.test.paidy.io/nexus/content/repositories/releases
+      '';
       shellAliases = {
         vpn = "nix run git+ssh://git@github.com/paidy/openvpn3-manager-nix";
         race_condish = "cd Paidy/set-bau-scripts/reprocess_race_conditions/ && nix develop --command bash -c 'DELTA_HOURS=72 ./reprocess.sh'";
