0268e8f08c79ee9295ff248cfee010a52b05bb1f
Kamuela Dietiker <kamuela.dietiker@paidy.com>
2024-10-24 10:15:23 +0900

    better organization

--- home.nix
+++ home.nix
@@ -27,16 +27,7 @@
 
     home-manager.enable = true;
 
-    bash = {
-      enable = true;
-      initExtra = ''
-      export NEXUS_HOST_RELEASES=https://nexus.test.paidy.io/nexus/content/repositories/releases
-      '';
-      shellAliases = {
-        vpn = "nix run git+ssh://git@github.com/paidy/openvpn3-manager-nix";
-        race_condish = "cd Paidy/set-bau-scripts/reprocess_race_conditions/ && nix develop --command bash -c 'DELTA_HOURS=72 ./reprocess.sh'";
-      };
-    };
+    bash.enable = true;
 
     fzf = {
       enable = true;
--- work-laptop-home.nix
+++ work-laptop-home.nix
@@ -9,7 +9,19 @@
     packages = (with pkgs; [
     ]);
   };
-  
+  programs = {
+    
+    bash = {
+      initExtra = ''
+      export NEXUS_HOST_RELEASES=https://nexus.test.paidy.io/nexus/content/repositories/releases
+      '';
+      shellAliases = {
+        vpn = "nix run git+ssh://git@github.com/paidy/openvpn3-manager-nix";
+        race_condish = "cd Paidy/set-bau-scripts/reprocess_race_conditions/ && nix develop --command bash -c 'DELTA_HOURS=72 ./reprocess.sh'";
+      };
+    };
+  };
+
   imports = [
     ./home.nix
   ];
